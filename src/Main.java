fc8bc38b032dc45701ad6a5bda11291ccd07709b
Yting27 <ngyen713@gmail.com>
2018-07-28 13:44:51 +0800

    Add comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+*   main Program
+**/
+
 public class Main {
     public static void main(String[] args){
         Random rnd = new Random();
